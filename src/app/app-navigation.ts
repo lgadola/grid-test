7aff667497c69ae81d3cf81c44d23cef5cfe2cb2
lgadola <lukas@gadola.net>
2022-07-30 18:28:51 +0200

    demo version for dx support

---
+++ src/app/app-navigation.ts
@@ -0,0 +1,30 @@
+export const navigation = [
+  {
+    text: 'Home',
+    path: '/home',
+    icon: 'home'
+  },
+  {
+    text: 'Examples',
+    icon: 'folder',
+    items: [
+      {
+        text: 'Profile',
+        path: '/profile'
+      },
+      {
+        text: 'Tasks',
+        path: '/tasks'
+      },
+      {
+        text: 'Calendar',
+        path: '/calendar'
+      },
+    ]
+  },
+  {
+    text: 'Test',
+    path: '/pages/test',
+    icon: 'folder'
+  }
+];
